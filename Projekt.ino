409ba9a75778b997e437f6977819ddd827c360e0
Mackaronie <stremmchen@web.de>
2026-04-20 14:12:45 +0200

    Add files via upload

---
+++ Projekt.ino
@@ -0,0 +1,88 @@
+//Libaries
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+//Sensorpins
+int Temperatursensor = A0;
+int Lichtschalter = 2;
+int Licht = 3;
+int Piezzo = 6;
+
+//Sensorwerte
+int RohOuputTemperatur = 0;
+int temperatur;
+int tasterstatus = 0;
+
+//Zeiten
+int TemperaturUpdateDelay = 500;
+
+//Andere Variablien
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+int Feuertemp = 10;
+bool Lichtstatus;
+bool Feuer = false; //gerade kein Feuer hoffentlich
+
+void setup() {
+  Serial.begin(9600);
+
+  //Monitor starten
+  lcd.init();
+  lcd.backlight();
+
+  //Pins festlegen
+  pinMode(Lichtschalter, OUTPUT);
+  pinMode(Licht, OUTPUT);
+
+  //Variablen festlegen
+  Lichtstatus = false; //Licht ist gerade aus;
+}
+
+void loop() {
+
+  Temperatursystem();
+
+  Lichtsystem();
+
+  Feuerarlam();
+
+
+  //Monitor
+  lcd.setCursor(0,0);
+  lcd.print(temperatur);
+}
+
+void Lichtsystem() {
+  tasterstatus = digitalRead(Lichtschalter);
+
+  if(tasterstatus == HIGH) {
+    if(Lichtstatus == true) {
+      digitalWrite(Licht, LOW);
+      Lichtstatus = false;
+    } else {
+      digitalWrite(Licht, HIGH);
+      Lichtstatus = true;
+    }
+  }
+}
+
+void Temperatursystem() {
+  RohOuputTemperatur = analogRead(Temperatursensor);
+  temperatur = map(RohOuputTemperatur, 0, 410, -50, 150);
+  delay(TemperaturUpdateDelay);
+}
+
+void Feuerarlam() {
+  if(temperatur >= Feuertemp) {
+    Feuer = true;
+  } else {
+    Feuer = false;
+  }
+
+  Serial.print(Feuer);
+
+  if(Feuer == true) {
+    digitalWrite(Piezzo, HIGH);
+    delay(500);
+    digitalWrite(Piezzo, LOW);
+  }
+}
