f61449b32616b868f0bdeaec39b3cc319e0f24db
Mackaronie <stremmchen@web.de>
2026-05-04 15:18:57 +0200

    w

--- Projekt/Temperatur_.ino
+++ Projekt/Temperatur_.ino
@@ -1,3 +1,7 @@
+//-------------------------------//
+//WPU Projekt von Aaron und David//
+//-------------------------------//
+
 void Temperatursystem() {
   RohOuputTemperatur = analogRead(Temperatursensor);
   temperatur = map(RohOuputTemperatur, 0, 410, -50, 150);
